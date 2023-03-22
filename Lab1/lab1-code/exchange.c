938ccf55dbd6c304c3a87320161917a766d6ed33
Swarleyman1 <j.malmenstedt@gmail.com>
2023-03-22 17:04:00 +0100

    Lab1

--- Lab1/lab1-code/exchange.c
+++ Lab1/lab1-code/exchange.c
@@ -19,15 +19,28 @@ int main(int argc, char *argv[]) {
   a = 100.0 + (double) rank;  /* Different a on different processors */
 
   /* Exchange variable a, notice the send-recv order */
+
   if (rank == 0) {
-    MPI_Send(&a, 1, MPI_DOUBLE, 1, 111, MPI_COMM_WORLD);
-    MPI_Recv(&b, 1, MPI_DOUBLE, 1, 222, MPI_COMM_WORLD, &status);
-    printf("Processor 0 got %f from processor 1\n", b);
-  } else if (rank==1) {
-    MPI_Recv(&b, 1, MPI_DOUBLE, 0, 111, MPI_COMM_WORLD, &status);
-    MPI_Send(&a, 1, MPI_DOUBLE, 0, 222, MPI_COMM_WORLD);
-    printf("Processor 1 got %f from processor 0\n", b);
+    MPI_Send(&a, 1, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
+    printf("Processor 0 sent %f to processor 1\n", a);
+    MPI_Recv(&b, 1, MPI_DOUBLE, size-1, size-1, MPI_COMM_WORLD, &status);
+    printf("Processor 0 got %f from processor %d\n", b, size-1);
   }
+
+  for (int i = 1; i < size-1; i++) {
+    if (rank==i){
+      MPI_Recv(&b, 1, MPI_DOUBLE, i-1, i, MPI_COMM_WORLD, &status);
+      printf("Processor %d got %f from processor %d...\n ...and sent %f to processor %d\n", rank, b, rank-1, a, rank+1);
+      MPI_Send(&a, 1, MPI_DOUBLE, i+1, i+1, MPI_COMM_WORLD);
+    }
+  } 
+
+  if (rank==size-1){
+    MPI_Recv(&b, 1, MPI_DOUBLE, size-2, size-1, MPI_COMM_WORLD, &status);
+    printf("Processor %d got %f from processor %d...\n ...and sent %f to processor %d\n", rank, b, rank-1, a, 0);
+    MPI_Send(&a, 1, MPI_DOUBLE, 0, size-1, MPI_COMM_WORLD);
+  }
+  
 
   MPI_Finalize(); 
 
--- Lab1/lab1-code/hello.c
+++ Lab1/lab1-code/hello.c
@@ -8,9 +8,19 @@
 
 int main(int argc, char *argv[]) {
 
+  int size, rank;
+
   MPI_Init(&argc, &argv);               /* Initialize MPI               */
 
-  printf("Hello World!\n");             /* Print a message              */
+  MPI_Comm_size(MPI_COMM_WORLD, &size); /* Get the number of processes  */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* Get the rank of this process */
+
+
+  if (rank == 0) {                      /* Only the first process prints this  */
+    printf("There are %d processes\n", size);
+  }
+
+  printf("Hello from %d!\n", rank);     /* Print a message              */
 
   MPI_Finalize();                       /* Shut down and clean up MPI   */
 
