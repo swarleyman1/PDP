8f05e08a97ad0d2f3dafe3823a02808c3e64513d
swarleyman1 <j.malmenstedt@gmail.com>
2023-04-13 14:52:52 +0200

    end of lab 3

--- Lab3/array_test.c
+++ Lab3/array_test.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    n = 100;  // size of the 2D array
+    n = 10;  // size of the 2D array
     k = n / size;  // number of rows per process
 
     // Allocate local 2D array
