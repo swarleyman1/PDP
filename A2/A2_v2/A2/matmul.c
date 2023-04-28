775ae80de182cba02c1750419da043226281dff5
swarleyman1 <j.malmenstedt@gmail.com>
2023-04-28 10:57:08 +0200

    Some optimizations and bugfixes

--- A2/A2_v2/A2/matmul.c
+++ A2/A2_v2/A2/matmul.c
@@ -9,14 +9,12 @@
 // number of processes must be a perfect square
 
 /* TODO: 
-Swap A and B to get correct output
-Improve matrix multiplication inner loop
-Remove mallocs and frees?
+
 
 */
 
 
-int write_output(char *file_name, const float *output, int n, int size)
+int write_output(char *file_name, const double *output, int n, int size)
 {
     // Function that writes the output in an output file
     // (transposes the matrix to get the correct output)
@@ -65,7 +63,7 @@ int main(int argc, char **argv)
     // Initialize variables
     char *input = argv[1];
     char *output = argv[2];
-    float *A, *B, *C, *A_chunk, *B_chunk, *C_chunk;
+    double *A, *B, *C, *A_chunk, *B_chunk, *C_chunk;
     int n;
 
     // Read input file
@@ -77,17 +75,17 @@ int main(int argc, char **argv)
         x = fscanf(file, "%d", &n);
 
         // Allocate memory for global matrices A, B and C
-        A = malloc(n * n * sizeof(float));
-        B = malloc(n * n * sizeof(float));
-        C = malloc(n * n * sizeof(float));
+        A = malloc(n * n * sizeof(double));
+        B = malloc(n * n * sizeof(double));
+        C = malloc(n * n * sizeof(double));
 
         for (int i = 0; i < (n * n); i++)
         {
-            x = fscanf(file, "%f", &A[i]);
+            x = fscanf(file, "%lf", &B[i]); // Quick fix to swap matrices A and B
         }
         for (int i = 0; i < (n * n); i++)
         {
-            x = fscanf(file, "%f", &B[i]);
+            x = fscanf(file, "%lf", &A[i]);
         }
     }
 
@@ -100,12 +98,16 @@ int main(int argc, char **argv)
     int num_rows = n / size;
 
     // Allocate memory for local matrices A_chunk, B_chunk and C_chunk
-    A_chunk = malloc(n * num_rows * sizeof(float));
-    B_chunk = malloc(n * num_rows * sizeof(float));
-    C_chunk = malloc(num_rows * num_rows * sizeof(float));
+    A_chunk = malloc(n * num_rows * sizeof(double));
+    B_chunk = malloc(n * num_rows * sizeof(double));
+    C_chunk = malloc(num_rows * num_rows * sizeof(double));
+
+    // Allocate memory for temporary matrices
+    double *temp1 = malloc(num_rows * sizeof(double));
+    double *temp2 = malloc(num_rows * sizeof(double));
 
     // Scatter rows of B to  the different processes
-    MPI_Scatter(B, n * num_rows, MPI_FLOAT, B_chunk, n * num_rows, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(B, n * num_rows, MPI_DOUBLE, B_chunk, n * num_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     for (int l = 0; l < size; l++) // Loop over chunks of A
     {
@@ -122,10 +124,12 @@ int main(int argc, char **argv)
         }
 
         // broadcast the chunk of columns of A to all processes
-        MPI_Bcast(A_chunk, n * num_rows, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(A_chunk, n * num_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         // do matrix multiplication
-        memset(C_chunk, 0, num_rows * num_rows * sizeof(float));
+        memset(C_chunk, 0, num_rows * num_rows * sizeof(double));
+
+        double temp = 0;
 
         for (int j = 0; j < num_rows; ++j)
         {
@@ -133,8 +137,10 @@ int main(int argc, char **argv)
             {
                 for (int i = 0; i < n; ++i)
                 {
-                    C_chunk[(j * num_rows + k)] += A_chunk[j * n + i] * B_chunk[n * k + i];
+                    temp += A_chunk[j * n + i] * B_chunk[n * k + i];
                 }
+                C_chunk[(j * num_rows + k)] = temp;
+                temp = 0;
             }
         }
 
@@ -145,13 +151,14 @@ int main(int argc, char **argv)
             {
                 for (int r = 1; r < size; r++) // Receive from all processes (note: will not execute if serial)
                 {
-                    float *temp1 = malloc(num_rows * sizeof(float)); // ----------------------------- Not ideal
-                    MPI_Recv(temp1, num_rows, MPI_FLOAT, r, 0, MPI_COMM_WORLD, &status);
+                    //double *temp1 = malloc(num_rows * sizeof(double)); // ----------------------------- Not ideal
+                    MPI_Recv(temp1, num_rows, MPI_DOUBLE, r, 0, MPI_COMM_WORLD, &status);
                     for (int s = 0; s < num_rows; s++)
                     {
                         C[(r * num_rows + q * n) + (l * n * num_rows) + s] = temp1[s];
                     }
-                    free(temp1);
+                    //free(temp1);
+                    memset(temp1, 0, num_rows * sizeof(double));
                     for (int s = 0; s < num_rows; s++)
                     {
                         C[(q * n) + (l * n * num_rows) + s] = C_chunk[s + q * num_rows];
@@ -160,13 +167,14 @@ int main(int argc, char **argv)
             }
             else
             {
-                float *temp2 = malloc(num_rows * sizeof(float)); // ----------------------------- Not ideal
+                //double *temp2 = malloc(num_rows * sizeof(double)); // ----------------------------- Not ideal
                 for (int s = 0; s < num_rows; s++)
                 {
                     temp2[s] = C_chunk[q * num_rows + s];
                 }
-                MPI_Send(temp2, num_rows, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-                free(temp2);
+                MPI_Send(temp2, num_rows, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+                //free(temp2);
+                memset(temp2, 0, num_rows * sizeof(double));
             }
         }
     }
